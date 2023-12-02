589db500ef15ec6cb92c45915e3f470eae9f5bff
Patrique Ouimet <patrique.ouimet@gmail.com>
2023-12-02 07:22:46 -0500

    chore: reorganize

---
+++ day_1/part_1.php
@@ -0,0 +1,19 @@
+<?php
+
+$calibrations = preg_split(
+    "/\r\n|\n|\r/",
+    file_get_contents(__DIR__.'/input.txt')
+);
+
+$total = 0;
+
+foreach ($calibrations as $calibration) {
+    $nums = preg_replace('/[^0-9]/', '', $calibration);
+    $first = $nums[0] ?? '';
+    $last = $nums[strlen($nums)-1] ?? '';
+    $num = sprintf('%s%s', $first, $last);
+    $total += (int) $num;
+}
+
+echo "TOTAL: $total\n";
+
