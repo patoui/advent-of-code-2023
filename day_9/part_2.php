fa43fbbd618f070d3fb48ef63812916944e8dd56
Patrique Ouimet <patrique.ouimet@gmail.com>
2023-12-25 07:22:24 -0500

    chore: completed day 9 part 2

---
+++ day_9/part_2.php
@@ -0,0 +1,48 @@
+<?php
+
+$lines = preg_split(
+    "/\n/",
+    file_get_contents(__DIR__.'/input.txt')
+);
+
+function tree($ns, $c = []) {
+    $nset = [];
+
+    for ($i = 0; $i < count($ns) - 1; $i++) {
+        $nset[] = $ns[$i+1] - $ns[$i];
+    }
+
+    $c[] = $nset;
+
+    if (! array_filter($nset)) {
+        return $c;
+    }
+
+    return tree($nset, $c);
+}
+
+function prev_val($tree, $nv = 0) {
+    $ct = array_pop($tree);
+    $treec = count($tree);
+    $tprev = $tree[$treec-1];
+
+    $nv = $tprev[0] - $ct[0];
+
+    if ($treec === 1) {
+        return $nv;
+    }
+
+    array_unshift($tree[$treec-1], $nv);
+
+    return prev_val($tree, $nv);
+}
+
+$sum = 0;
+
+foreach ($lines as $line) {
+    $nums = array_map('intval', explode(' ', $line));
+    $tree = tree($nums, [$nums]);
+    $sum += prev_val($tree);
+}
+
+echo "SUM: $sum\n";
