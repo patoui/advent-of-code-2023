0941efa29aa7e5e936df5c8f2742d465abd58aed
Patrique Ouimet <patrique.ouimet@gmail.com>
2023-12-27 21:34:03 -0500

    chore: revise day 11 part 1

--- day_11/part_1.py
+++ day_11/part_1.py
@@ -3,7 +3,6 @@ from collections import defaultdict
 input = open('input.txt', 'r')
 lines = input.readlines()
 
-grid = []
 galaxies = []
 erows = defaultdict(int)
 ecols = defaultdict(int)
@@ -12,42 +11,16 @@ ecols = defaultdict(int)
 for y in range(0, len(lines)):
     erows[y] += 0
     cols = list(lines[y].strip())
-    grid.append(cols)
     for x in range(0, len(cols)):
         ecols[x] += 0
         if cols[x] == '#':
             erows[y] += 1
             ecols[x] += 1
+            galaxies.append((x, y))
 
 # remove non-zero dictionary entries
 erows = {k:v for (k,v) in erows.items() if v == 0}
 ecols = {k:v for (k,v) in ecols.items() if v == 0}
-# print("EROWS {} | ECOLS {}".format(erows, ecols))
-
-# expand grid horitonzally
-for y in range(0, len(grid)):
-    xo = 0
-    for x in ecols.keys():
-        grid[y].insert(x + xo, '.')
-        xo += 1
-
-colcount = len(grid[0])
-
-yo = 0
-
-# expand grid vertically
-for k in erows.keys():
-    grid.insert(k + yo, ['.'] * colcount)
-    yo += 1
-
-# for g in grid:
-#     print(''.join(g))
-
-# find galaxy positions
-for y in range(0, len(grid)):
-    for x in range(0, len(grid[y])):
-        if grid[y][x] == '#':
-            galaxies.append((x, y))
 
 total = 0
 gc = 0
@@ -55,10 +28,21 @@ gc = 0
 # calculate galaxy distances
 for g in galaxies:
     for ig in range(gc + 1, len(galaxies)):
-        x = abs(g[0] - galaxies[ig][0])
-        y = abs(g[1] - galaxies[ig][1])
+        xmax = max(g[0], galaxies[ig][0])
+        xmin = min(g[0], galaxies[ig][0])
+        x = xmax - xmin
+        for k in range(xmin, xmax):
+            if k in ecols:
+                x += 1
+
+        ymax = max(g[1], galaxies[ig][1])
+        ymin = min(g[1], galaxies[ig][1])
+        y = galaxies[ig][1] - g[1]
+        for k in range(ymin, ymax):
+            if k in erows:
+                y += 1
+
         total += x + y
     gc += 1
 
-# print("GALAXIES {}".format(galaxies))
 print("TOTAL {}".format(total))
