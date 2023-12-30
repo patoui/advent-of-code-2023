e1e086b0b579d2b0388d3dc3024f8d575e9edeb3
Patrique Ouimet <patrique.ouimet@gmail.com>
2023-12-30 07:29:07 -0500

    chore: completed day 12 part 1

--- day_12/part_1.py
+++ day_12/part_1.py
@@ -1,10 +1,11 @@
 import re
 
-input = open('example.txt', 'r')
+input = open('input.txt', 'r')
 lines = input.readlines()
 
-def can_replace(rec, i, c):
-    recp = rec[i-1] if 0 <= i-1 < len(rec) else None
+# TODO: simplify and clean up
+def can_replace(rec, i, c, is_last = False):
+    recp = rec[i-1] if i-1 >= 0 else None
 
     # check prev char
     if recp is not None and recp != '.' and recp != '?':
@@ -16,29 +17,48 @@ def can_replace(rec, i, c):
     if recn is not None and recn != '.' and recn != '?':
         return False
 
-    exp = r'[#|?]{' + re.escape(str(c)) + r'}'
-    pattern = re.compile(exp)
-
     # retrieve substring to check
     r = rec[i:i+c]
 
     # check substring pattern is replaceable
-    return bool(pattern.search(r))
+    is_valid = re.compile(r'[#|?]{' + re.escape(str(c)) + r'}').search(r)
+
+    if is_valid is None:
+        return False
+
+    if i == 0:
+        return True
+
+    # retrieve everything before current index
+    b = rec[0:i-1]
+
+    # check no values before current index have not been replaced
+    if re.compile(r'#+').search(b) is not None:
+        return False
+
+    if is_last == True:
+        # retrieve everything after current index + condition count
+        a = rec[i+c:len(rec)-1]
+
+        if len(a) > 0:
+            ac = re.compile(r'#+').search(a)
+            return ac is None
+
+    return True
 
 def possible_variations(sets, conditions):
     if len(conditions) == 0:
-        return sets
+        return [x for x in sets if '#' not in x[0]]
 
     c = int(conditions.pop(0))
     prev_record = [s[0] for s in sets]
     new_sets = []
+    is_last = len(conditions) == 0
 
-    for s in sets:
-        (record, start_index) = s
-
+    for record, start_index in sets:
         # calculate number of possible variations
         for i in range(start_index, len(record)):
-            if can_replace(record, i, c):
+            if can_replace(record, i, c, is_last):
                 new_record = record[:i] + ('$' * c) + record[i+c:]
                 if new_record not in prev_record:
                     prev_record.append(new_record)
@@ -57,6 +77,9 @@ for line in lines:
 
     sum += len(pvars)
 
+# 8270, correct
+# 8451, too high
+# 10761, incorrect
 # 11025, too high
 # 11253, too high
 # 13948, too high
